d5cafc23618139ab24fb4b095d4dda93a8de39fa
Sandesh Dadibude <sandeshdadibude28042004@.com>
2026-01-23 15:17:32 +0530

    added all controllers and some security cofiguration files like security files

--- Spring-Boot-Security/spring-security/src/main/java/com/example/spring_security/model/User.java
+++ Spring-Boot-Security/spring-security/src/main/java/com/example/spring_security/model/User.java
@@ -4,13 +4,19 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +26,38 @@ public class User {
     private String gender;
     private int age;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- Spring-Boot-Security/spring-security/src/main/java/com/example/spring_security/repository/UserRepository.java
+++ Spring-Boot-Security/spring-security/src/main/java/com/example/spring_security/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.spring_security.repository;
 
 import com.example.spring_security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByName(String username);
 }
