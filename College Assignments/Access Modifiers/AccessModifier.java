8f7222ba009b43fc8efc96e8519cbb5e7aea3dc0
unknown <sandeshdadibude28042004@gmail.com>
2026-02-28 11:39:52 +0530

    added access modifiers and ternary operator programs

---
+++ College Assignments/Access Modifiers/AccessModifier.java
@@ -0,0 +1,14 @@
+class Box{
+
+		public int width=10;
+		private int height=20;
+}
+
+class AccessModifier{
+		
+		public static void main (String agrs[]){
+			Box box=new Box();
+			System.out.println(box.width);
+		}
+
+}
